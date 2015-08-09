5b9e5b5006c240d35086fbbd56da8ca2e021cb6c
maclover7 <me@jonathanmoss.me>
2015-08-08 20:33:33 -0400

    Generated user model

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,4 +2,10 @@ require 'faker'
 require 'factory_girl_rails'
 
 FactoryGirl.define do
+  factory :user do
+    email { Faker::Internet.email }
+    name { Faker::Name.name }
+    password "foobarfoobar"
+    password_confirmation { |u| u.password }
+  end
 end
