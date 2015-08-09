a6442cbf9e41af3989e8941d6026d81a10315af8
maclover7 <me@jonathanmoss.me>
2015-08-08 21:55:21 -0400

    Permit name parameter into Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  before_action :configure_devise_params, if: :devise_controller?
   before_action :authenticate_user!
+
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
+  end
 end
